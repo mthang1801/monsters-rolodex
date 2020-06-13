998e6d895243245bdb1e6c680759653b0a672b2d
mthang1801 <mthang1801@gmail.com>
2020-06-13 14:31:32 +0700

    Updates

---
+++ precache-manifest.5fa94b61427b1333344eaffd0669e87f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc3f24c211f7bfc09703c7b37e4e7fc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e4cfc1ebb7d2ef47502b",
+    "url": "/monsters-rolodex/static/css/main.babd788c.chunk.css"
+  },
+  {
+    "revision": "96fd4dc3ef25187f5cac",
+    "url": "/monsters-rolodex/static/js/2.4229bec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.4229bec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4cfc1ebb7d2ef47502b",
+    "url": "/monsters-rolodex/static/js/main.46ac3e0f.chunk.js"
+  },
+  {
+    "revision": "0aba45cb0fad51f6f55b",
+    "url": "/monsters-rolodex/static/js/runtime-main.e15b991a.js"
+  }
+]);
